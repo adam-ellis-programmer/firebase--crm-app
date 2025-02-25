03a760f8c7027cc8347d085f5ad8596b830a6253
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-02-25 21:29:45 +0000

    rbac update

---
+++ src/pages/view data dash/AgentsData.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AgentsData = () => {
+  return (
+    <div>
+      list of customers belonging to that target agent
+      <ul>
+        <li>customer name</li>
+      </ul>
+    </div>
+  )
+}
+
+export default AgentsData
---
+++ src/pages/view data dash/ViewableAgents.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ViewableAgents = () => {
+  return (
+    <div>
+      list of all agents we have access to
+      <ul>
+        <li>customer name</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ViewableAgents
